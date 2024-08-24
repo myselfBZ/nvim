e671a40c1665f0f354735e9383bf81cd0f180ff2
Boburmirzo Alivobjonov <symonbobur@gmail.com>
2024-08-24 13:39:33 +0500

    feat:Something

--- init.lua
+++ init.lua
@@ -54,7 +54,10 @@ local plugins = {
             vim.keymap.set('i', '<c-x>', function() return vim.fn['codeium#Clear']() end, { expr = true, silent = true })
         end
     },
-    { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+    -- install rose pine
+    {
+        'rose-pine/neovim'
+    },
     {
         'nvim-telescope/telescope.nvim', tag = '0.1.6',
         -- or                              , branch = '0.1.x',
@@ -109,19 +112,96 @@ config.setup({
     highlight = { enable = true },
     indent = { enable = true },  
 })
-require("catppuccin").setup()
+-- set up my rose pine
+require("rose-pine").setup({
+    variant = "moon", -- auto, main, moon, or dawn
+    dark_variant = "main", -- main, moon, or dawn
+    dim_inactive_windows = false,
+    extend_background_behind_borders = true,
+
+    enable = {
+        terminal = true,
+        legacy_highlights = true, -- Improve compatibility for previous versions of Neovim
+        migrations = true, -- Handle deprecated options automatically
+    },
+
+    styles = {
+        bold = false,
+        italic = false,
+        transparency = false,
+    },
+
+    groups = {
+        border = "muted",
+        link = "iris",
+        panel = "surface",
+
+        error = "love",
+        hint = "iris",
+        info = "foam",
+        note = "pine",
+        todo = "rose",
+        warn = "gold",
+
+        git_add = "foam",
+        git_change = "rose",
+        git_delete = "love",
+        git_dirty = "rose",
+        git_ignore = "muted",
+        git_merge = "iris",
+        git_rename = "pine",
+        git_stage = "iris",
+        git_text = "rose",
+        git_untracked = "subtle",
+
+        h1 = "iris",
+        h2 = "foam",
+        h3 = "rose",
+        h4 = "gold",
+        h5 = "pine",
+        h6 = "foam",
+    },
+
+    pallete = {
+        -- Override the builtin palette per variant
+        -- moon = {
+        --     base = '#18191a',
+        --     overlay = '#363738',
+        -- },
+    },
+
+    highlight_groups = {
+        -- Comment = { fg = "foam" },
+        -- VertSplit = { fg = "muted", bg = "muted" },
+    },
+
+    before_highlight = function(group, highlight, palette)
+        -- Disable all undercurls
+        -- if highlight.undercurl then
+        --     highlight.undercurl = false
+        -- end
+        --
+        -- Change palette colour
+        -- if highlight.fg == palette.pine then
+        --     highlight.fg = palette.foam
+        -- end
+    end,
+})
+
+vim.cmd("colorscheme rose-pine")
+vim.cmd("colorscheme rose-pine-main")
+--vim.cmd("colorscheme rose-pine-dawn") 
 require("mason").setup()
 require("mason-lspconfig").setup{
     ensure_installed = {
-        "rust_analyzer",  -- Rust
-        "tsserver",       -- JavaScript
-        "pyright",        -- Python
+        "tsserver",
+        "pyright",
+        "gopls",
     }
 }
 
 
 
-vim.cmd("colorscheme catppuccin")
 
 local lsp_config = require("lspconfig")
 local capabilities = require('cmp_nvim_lsp').default_capabilities()
